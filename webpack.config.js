ad6e6e88f9afa1e4dc37d0aab28995d752876063
nero-15 <nero15.dev@gmail.com>
2021-06-06 23:25:59 +0900

    setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	entry: './src/app.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+		publicPath: '/',
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist')
+	}
+}
