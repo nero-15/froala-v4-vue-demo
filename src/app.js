0b913759dcab9d9c6771f574ffc10c7fd47257f2
nero-15 <nero15.dev@gmail.com>
2021-06-06 23:42:02 +0900

    import vue

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
-init();
+// Require Froala Editor js file.
+require('froala-editor/js/froala_editor.pkgd.min.js')
 
-function init() {
-	console.log('hello world');
-}
+// Require Froala Editor css files.
+require('froala-editor/css/froala_editor.pkgd.min.css')
+require('froala-editor/css/froala_style.min.css')
+
+// Import and use Vue Froala lib.
+import VueFroala from 'vue-froala-wysiwyg'
+Vue.use(VueFroala)
+new Vue({
+	render: h => h(App),
+
+}).$mount('#app')
