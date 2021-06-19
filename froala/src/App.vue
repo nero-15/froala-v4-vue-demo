a79fff7e489ebc53ea853c566b253a646d61e089
nero-15 <nero15.dev@gmail.com>
2021-06-19 22:37:33 +0900

    modify app.vue

--- froala/src/App.vue
+++ froala/src/App.vue
@@ -2,16 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <froala :tag="'textarea'" :config="config" v-model="model"></froala>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VueFroala from 'vue-froala-wysiwyg';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      config: {
+        events: {
+          'froalaEditor.initialized': function () {
+            console.log('initialized')
+          }
+        }
+      },
+      model: 'Edit Your Content Here!'
+    }
   }
 }
 </script>
