4f86bbb5eb85d7fa49e1b47129900eb16e7e1d6e
nero-15 <nero15.dev@gmail.com>
2021-06-20 23:11:25 +0900

    fix

--- froala/src/main.js
+++ froala/src/main.js
@@ -1,23 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/dashboard.css";
 
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/css/froala_style.min.css";
+
+// JS
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // import "feather-icons/dist/feather.min.js"; //TODO
-import "chart.js/dist/chart.min.js";
+import "chart.js/dist/chart.min.js"; //TODO
 import "./assets/js/dashboard.js";
 
-// Require Froala Editor js file.
-require('froala-editor/js/froala_editor.pkgd.min.js')
-
-// Require Froala Editor css files.
-require('froala-editor/css/froala_editor.pkgd.min.css')
-require('froala-editor/css/froala_style.min.css')
-
-// Import and use Vue Froala lib.
+import "froala-editor/js/froala_editor.pkgd.min.js";
 import VueFroala from 'vue-froala-wysiwyg'
+
 Vue.use(VueFroala)
 
 Vue.config.productionTip = false
