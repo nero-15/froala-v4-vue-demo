1a239dc0956fc8e53f980a15d09948d256ec3f6b
nero-15 <nero15.dev@gmail.com>
2021-06-21 22:21:31 +0900

    fix

--- froala/src/App.vue
+++ froala/src/App.vue
@@ -6,7 +6,6 @@
                 <LeftNavi></LeftNavi>
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                     <H1></H1>
-                    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
                     <h2>Section title</h2>
                     <img alt="Vue logo" src="./assets/logo.png">
                     <HelloWorld msg="Welcome to Your Vue.js App"/>
