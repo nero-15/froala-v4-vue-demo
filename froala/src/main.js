d367efa41141fad1b9dd8fca0191f2f53b976471
nero-15 <nero15.dev@gmail.com>
2021-06-27 21:42:40 +0900

    setting init

--- froala/src/main.js
+++ froala/src/main.js
@@ -3,6 +3,35 @@ import VueFroala from 'vue-froala-wysiwyg'
 import App from './App.vue'
 import router from './router.js'
 
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJVlSytsb5anBHGvGTuY3u4KYLEWn3uME",
+    authDomain: "froala-v4-vue-demo.firebaseapp.com",
+    projectId: "froala-v4-vue-demo",
+    storageBucket: "froala-v4-vue-demo.appspot.com",
+    messagingSenderId: "524881768806",
+    appId: "1:524881768806:web:f511a5a40a773ae31618a7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
+
+
 // CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/dashboard.css";
