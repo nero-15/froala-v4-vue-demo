5ca43ee9d51eed21ce66b74efef81f7b83095867
nero-15 <nero15.dev@gmail.com>
2021-06-29 22:30:42 +0900

    setting logout

--- froala/src/components/Header.vue
+++ froala/src/components/Header.vue
@@ -3,16 +3,23 @@
         <router-link to="/" class="navbar-brand col-md-3 col-lg-2 me-0 px-3">froala v4</router-link>
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
-                <a class="nav-link" href="#">Sign out</a>
+                <a class="nav-link" href="javascript:void(0);" @click="logout()">Sign out</a>
             </li>
         </ul>
     </header>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
     name: 'Header',
     props: {
+    },
+    methods: {
+        logout: function() {
+            firebase.auth().signOut();
+        }
     }
 }
 </script>
