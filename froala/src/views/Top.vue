46a8c102996887bc5dd2ba1f957545a0ab4ccf23
nero-15 <nero15.dev@gmail.com>
2021-07-02 22:17:32 +0900

    add table

--- froala/src/views/Top.vue
+++ froala/src/views/Top.vue
@@ -1,11 +1,31 @@
 <template>
     <div>
-        Top
+        <h2>articles</h2>
+        <div class="table-responsive">
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>title</th>
+                        <th>created</th>
+                        <th>detail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1,001</td>
+                        <td>Lorem</td>
+                        <td>ipsum</td>
+                        <td>link</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Top'
+    name: 'Top'
 }
 </script>
