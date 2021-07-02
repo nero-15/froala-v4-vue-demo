9b883b4341a3474b6985d658b696b587d500fa7c
nero-15 <nero15.dev@gmail.com>
2021-07-02 22:00:38 +0900

    fix

--- froala/src/views/Create.vue
+++ froala/src/views/Create.vue
@@ -8,6 +8,12 @@
 
 <script>
 import firebase from 'firebase'
+import Vue from 'vue'
+import VueFroala from 'vue-froala-wysiwyg'
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/css/froala_style.min.css";
+import "froala-editor/js/froala_editor.pkgd.min.js";
+Vue.use(VueFroala)
 
 export default {
     name: 'Create',
